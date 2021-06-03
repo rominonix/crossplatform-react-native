8c4adcad7f953c5cfd5357b9f27b06f4291a72b1
Romina Rebolledo <rominarebolledo@gmail.com>
2021-06-03 11:29:19 +0200

    delete MyComponent and edit app.json

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { StatusBar } from 'expo-status-bar'
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import SplashScreen from './components/SplashScreen'
-import MyComponent from './components/MyComponent'
 import TheRestOfTheApp from './components/TheRestOfTheApp'
 import { useState } from 'react'
 
