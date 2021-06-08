18a18a7d20314d6700429fe992aa6bc007e91e9d
Romina Rebolledo <rominarebolledo@gmail.com>
2021-06-08 21:23:45 +0200

    update login, loginContext, index

--- src/api/index.js
+++ src/api/index.js
@@ -1,24 +1,26 @@
 import axios from 'axios'
 
-
 const HOST = '10.0.2.2'
 const API = axios.create(
- {baseUrl:`http://${HOST}:1337/api/v6`}
+   { baseUrl: `http://${HOST}:3000` }
 )
 
-export async function fetchUsers(){
-   const response = await API.post('/login')
-   if(response.code == 200){ return response.data }
-   else{
-      response.error({msg: "unauthenticate"})
+export async function authenticate(email, password) {
+
+   const response = await API.post('/login',
+      { email, password })
+
+   if (response.code == 200) { return response.data }
+   else {
+      response.error({ msg: "invalid credentials" })
    }
-  }
-
-
-export async function fetchLatestTasks(){
- const response = await API.get('/tasks')
- if(response.code == 200){ return response.data }
- else{
-    response.error({msg: "error with tasks"})
- }
 }
+
+
+// export async function fetchLatestTasks(){
+//  const response = await API.get('/tasks')
+//  if(response.code == 200){ return response.data }
+//  else{
+//     response.error({msg: "error with tasks"})
+//  }
+// }
